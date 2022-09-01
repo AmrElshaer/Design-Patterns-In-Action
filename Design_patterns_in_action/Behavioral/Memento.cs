8ac74cded90d42e1cb7e626fe9b846338e776316
Amr Elshaer <amrelsher07@gmail.com>
2022-09-01 13:01:28 +0200

    add memento design pattern

---
+++ Design_patterns_in_action/Behavioral/Memento.cs
@@ -0,0 +1,46 @@
+﻿namespace Design_patterns_in_action.Behavioral
+{
+    public class EditorState
+    {
+        public EditorState(string content)
+        {
+            Content = content;
+        }
+
+        public string Content { get; init; }
+
+    }
+    public class Editor
+    {
+        public string Content { get; private set; }
+
+        public void EditContent(string content)
+        {
+            this.Content = content;
+        }
+        public EditorState CreatEditorState()
+        {
+            return new EditorState(Content);
+        }
+
+        public void RestoreState(EditorState state)
+        {
+            this.Content = state.Content;
+        }
+    }
+
+    public class History
+    {
+        private readonly Stack<EditorState> states = new Stack<EditorState>();
+
+        public void AddState(EditorState state)
+        {
+            states.Push(state);
+        }
+
+        public EditorState RemoveState()
+        {
+            return states.Pop();
+        }
+    }
+}
--- Design_patterns_in_action/Program.cs
+++ Design_patterns_in_action/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Design_patterns_in_action.Behavioral;
+
+Editor editor = new();
+History history = new History();
+editor.EditContent("a");
+history.AddState(editor.CreatEditorState());
+editor.EditContent("b");
+history.AddState(editor.CreatEditorState());
+editor.EditContent("c");
+editor.RestoreState(history.RemoveState());
+Console.WriteLine(editor.Content);
+
