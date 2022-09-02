3488714cf393bcecf5b8e749244bc953122f78ce
Amr Elshaer <amrelsher07@gmail.com>
2022-09-03 00:26:59 +0200

    composite command

---
+++ Design_patterns_in_action/Behavioral/CompositeCommand.cs
@@ -0,0 +1,40 @@
+﻿namespace Design_patterns_in_action.Behavioral
+{
+    public interface Command
+    {
+        void Execute();
+    }
+
+    class BlackAndWhiteCommand : Command
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Black and white command");
+        }
+    }
+
+    class ResizeCommand : Command
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Resize");
+        }
+    }
+
+    public class CompositeCommand
+    {
+        private IList<Command> _commands = new List<Command>();
+
+        public void AddCommand(Command command)
+        {
+            _commands.Add(command);
+        }
+        public void Execute()
+        {
+            foreach (var command in _commands)
+            {
+                command.Execute();
+            }
+        }
+    }
+}
--- Design_patterns_in_action/Program.cs
+++ Design_patterns_in_action/Program.cs
@@ -1,12 +1,7 @@
 ﻿using Design_patterns_in_action.Behavioral;
 
-Editor editor = new();
-History history = new History();
-editor.EditContent("a");
-history.AddState(editor.CreatEditorState());
-editor.EditContent("b");
-history.AddState(editor.CreatEditorState());
-editor.EditContent("c");
-editor.RestoreState(history.RemoveState());
-Console.WriteLine(editor.Content);
+CompositeCommand command = new CompositeCommand();
+command.AddCommand(new BlackAndWhiteCommand());
+command.AddCommand(new ResizeCommand());
+command.Execute();
 
