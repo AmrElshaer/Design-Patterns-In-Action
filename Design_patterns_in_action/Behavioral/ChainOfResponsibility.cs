3528254d718ff9c607c8f62c541fe92466968787
Amr Elshaer <amrelsher07@gmail.com>
2022-09-03 17:24:04 +0200

    chain of responsibility pattern

--- Design_patterns_in_action/Behavioral/ChainOfResponsibility.cs
+++ Design_patterns_in_action/Behavioral/ChainOfResponsibility.cs
@@ -1,5 +1,83 @@
 ﻿namespace Design_patterns_in_action.Behavioral
 {
+    public class WebServer
+    {
+        private Handler handler;
+
+        public WebServer(Handler handler)
+        {
+            this.handler = handler;
+        }
+
+        public void Handler(HttpRequest request)
+        {
+            handler.Handle(request);
+        }
+    }
+    public class HttpRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+    public class Authenticator : Handler
+    {
+        public Authenticator(Handler nextHandler) : base(nextHandler)
+        {
+        }
+
+        protected override bool DoHandler(HttpRequest request)
+        {
+            var isValid = (request.UserName == "admin" && request.Password == "admin");
+            Console.WriteLine("Authenticated");
+            return !isValid;
+        }
+    }
+    public class Compressor : Handler
+    {
+        public Compressor(Handler nextHandler) : base(nextHandler)
+        {
+        }
+
+        protected override bool DoHandler(HttpRequest request)
+        {
+
+            Console.WriteLine("Compress");
+            return false;
+        }
+    }
+    public class Logger : Handler
+    {
+        public Logger(Handler nextHandler) : base(nextHandler)
+        {
+        }
+
+        protected override bool DoHandler(HttpRequest request)
+        {
+
+            Console.WriteLine("Log");
+            return false;
+        }
+    }
+    public abstract class Handler
+    {
+        private readonly Handler next;
+
+        protected Handler(Handler nextHandler)
+        {
+            this.next = nextHandler;
+        }
+        // template pattern
+        public void Handle(HttpRequest request)
+        {
+            if (DoHandler(request))
+            {
+                return;
+            }
+            next?.Handle(request);
+
+        }
+        protected abstract bool DoHandler(HttpRequest request);
+    }
     // It helps building a chain of objects. Request enters from one end and keeps going from object to object till it finds the suitable handler.
     public class ChainOfResponsibility
     {
--- Design_patterns_in_action/Program.cs
+++ Design_patterns_in_action/Program.cs
@@ -1,4 +1,7 @@
 ﻿using Design_patterns_in_action.Behavioral;
-
-ArticlesDialogBox articlesDialogBox = new ArticlesDialogBox();
-articlesDialogBox.Simulate();
+//Auth=>log=>compressor
+var compressor = new Compressor(null);
+var logger = new Logger(compressor);
+var authenticator = new Authenticator(logger);
+var server = new WebServer(authenticator);
+server.Handler(new HttpRequest() { UserName = "admin", Password = "admin" });
