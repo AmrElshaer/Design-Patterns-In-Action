7fedc89944295637b99f13ed15b825acad32a8a1
Amr Elshaer <amrelsher07@gmail.com>
2022-09-04 13:58:48 +0200

    update

--- Design_patterns_in_action/Structural/Bridge.cs
+++ Design_patterns_in_action/Structural/Bridge.cs
@@ -1,6 +1,7 @@
 ﻿namespace Design_patterns_in_action.Structural
 {
     // Bridge pattern is about preferring composition over inheritance.
+    // Heroically split it using bridge , if the herocally of inherit is grow solve it using bridge
     //Consider you have a website with different pages and you are supposed to allow the user to change the theme.What would you do? Create multiple copies of each of the pages for each of the themes or would you just create separate theme and load them based on the user's preferences? Bridge pattern allows you to do the second i.e.
     interface IWebPage
     {
