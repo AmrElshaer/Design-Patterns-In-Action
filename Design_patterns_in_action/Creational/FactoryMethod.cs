5851638044cecb9f6367f2f28d6684a007e90c51
Amr Elshaer <50411156+AmrElshaer@users.noreply.github.com>
2023-03-20 08:50:05 +0200

    Update FactoryMethod.cs

--- Design_patterns_in_action/Creational/FactoryMethod.cs
+++ Design_patterns_in_action/Creational/FactoryMethod.cs
@@ -20,7 +20,7 @@ internal class CommunityExecutive : IInterviewer
         Console.WriteLine("Asking about community building!");
     }
 }
-
+//the definition of the Factory Method design pattern, which is when a superclass(HiringManager) provides an interface for creating objects(MakeInterviewerquestion method), but allows subclasses(Development,Marketing) to decide which class to instantiate. In this case, the HiringManager class provides a MakeInterviewer method that returns an instance of an IPaymentMethod interface, and delegates the responsibility of creating the appropriate IInterviewer implementation to its concrete subclasses, MarketingManager and DevelopmentManager.
 internal abstract class HiringManager
 {
     // Factory method
